0349aa4fd79235a12355de70ac6d1851dff37a46
albert.oclarit <bbjao143213620>
2016-08-17 16:14:43 +0800

    Cmon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var api = require('./src/TreeMenu.jsx');
 
-api.TreeMenu = require('./src/TreeMenu.jsx');
-api.TreeNode = require('./src/TreeNode.jsx');
+api.TreeMenu = require('./src/TreeMenu');
+api.TreeNode = require('./src/TreeNode');
 api.Utils = require('./src/TreeMenuUtils');
 
 module.exports = api;
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,15 @@ module.exports = {
     },
     devtool: 'sourcemap',
     plugins: [
+       /* new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new webpack.NoErrorsPlugin(),*/
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
